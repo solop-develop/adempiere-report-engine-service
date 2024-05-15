2351bdd353fdeda7821bebc895b115f33989b1dd
Yamel Senih <ysenih@erpya.com>
2024-05-15 11:24:45 -0400

    Initial commit

---
+++ docker/backend_focal.Dockerfile
@@ -0,0 +1,51 @@
+FROM eclipse-temurin:11.0.22_7-jdk-focal
+
+LABEL maintainer="ySenih@erpya.com; EdwinBetanc0urt@outlook.com;" \
+	description="Backend gRPC"
+
+# Init ENV with default values
+ENV \
+	SERVER_PORT="50059" \
+	SERVER_LOG_LEVEL="WARNING" \
+	DB_HOST="localhost" \
+	DB_PORT="5432" \
+	DB_NAME="adempiere" \
+	DB_USER="adempiere" \
+	DB_PASSWORD="adempiere" \
+	DB_TYPE="PostgreSQL" \
+	ADEMPIERE_APPS_TYPE="" \
+	TZ="America/Caracas"
+
+EXPOSE ${SERVER_PORT}
+
+
+# Add operative system dependencies
+RUN	apt-get update && \
+	apt-get install -y \
+		tzdata \
+		bash \
+	 	fontconfig \
+		ttf-dejavu && \
+	rm -rf /var/lib/apt/lists/* \
+	rm -rf /tmp/* && \
+	echo "Set Timezone..." && \
+	echo $TZ > /etc/timezone
+
+
+WORKDIR /opt/apps/server
+
+# Copy src files
+COPY docker/adempiere-grpc-template-service /opt/apps/server
+COPY docker/env.yaml /opt/apps/server/env.yaml
+COPY docker/start.sh /opt/apps/server/start.sh
+
+
+RUN addgroup adempiere && \
+	adduser --disabled-password --gecos "" --ingroup adempiere --no-create-home adempiere && \
+	chown -R adempiere /opt/apps/server/ && \
+	chmod +x start.sh
+
+USER adempiere
+
+# Start app
+ENTRYPOINT ["sh" , "start.sh"]
---
+++ src/main/java/org/spin/template/server/TemplateServer.java
@@ -0,0 +1,147 @@
+/************************************************************************************
+ * Copyright (C) 2012-2018 E.R.P. Consultores y Asociados, C.A.                     *
+ * Contributor(s): Yamel Senih ysenih@erpya.com                                     *
+ * This program is free software: you can redistribute it and/or modify             *
+ * it under the terms of the GNU General Public License as published by             *
+ * the Free Software Foundation, either version 2 of the License, or                *
+ * (at your option) any later version.                                              *
+ * This program is distributed in the hope that it will be useful,                  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                     *
+ * GNU General Public License for more details.                                     *
+ * You should have received a copy of the GNU General Public License                *
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.            *
+ ************************************************************************************/
+package org.spin.template.server;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.compiere.util.Env;
+import org.spin.service.grpc.authentication.AuthorizationServerInterceptor;
+import org.spin.service.grpc.context.ServiceContextProvider;
+import org.spin.template.controller.TemplateService;
+import org.spin.template.setup.SetupLoader;
+
+import io.grpc.Server;
+import io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.NettyServerBuilder;
+import io.netty.handler.ssl.ClientAuth;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.grpc.ServerBuilder;
+
+public class TemplateServer {
+	private static final Logger logger = Logger.getLogger(TemplateServer.class.getName());
+
+	private Server server;
+
+	private ServiceContextProvider contextProvider = new ServiceContextProvider();
+
+
+	/** Services/Methods allow request without Bearer token validation */
+	private List<String> ALLOW_REQUESTS_WITHOUT_TOKEN = Arrays.asList(
+		// proto package . proto service / proto method
+	);
+
+	/**	Revoke session	*/
+	private List<String> REVOKE_TOKEN_SERVICES = Arrays.asList(
+		// proto package . proto service / proto method
+	);
+
+	private AuthorizationServerInterceptor getInterceptor() {
+		AuthorizationServerInterceptor interceptor = new AuthorizationServerInterceptor();
+		interceptor.setAllowRequestsWithoutToken(this.ALLOW_REQUESTS_WITHOUT_TOKEN);
+		interceptor.setRevokeTokenServices(this.REVOKE_TOKEN_SERVICES);
+		return interceptor;
+	}
+
+
+	/**
+	 * Get SSL / TLS context
+	 * @return
+	 */
+	private SslContextBuilder getSslContextBuilder() {
+		SslContextBuilder sslClientContextBuilder = SslContextBuilder.forServer(
+			new File(SetupLoader.getInstance().getServer().getCertificate_chain_file()),
+			new File(SetupLoader.getInstance().getServer().getPrivate_key_file())
+		);
+		if (SetupLoader.getInstance().getServer().getTrust_certificate_collection_file() != null) {
+			sslClientContextBuilder.trustManager(
+				new File(SetupLoader.getInstance().getServer().getTrust_certificate_collection_file())
+			);
+			sslClientContextBuilder.clientAuth(ClientAuth.REQUIRE);
+		}
+		return GrpcSslContexts.configure(sslClientContextBuilder);
+	}
+
+	private void start() throws IOException {
+		//	Start based on provider
+		Env.setContextProvider(this.contextProvider);
+
+		logger.info("Service Template added on " + SetupLoader.getInstance().getServer().getPort());
+		//	
+		ServerBuilder<?> serverBuilder;
+		if(SetupLoader.getInstance().getServer().isTlsEnabled()) {
+			serverBuilder = NettyServerBuilder.forPort(SetupLoader.getInstance().getServer().getPort())
+				.sslContext(getSslContextBuilder().build());
+		} else {
+			serverBuilder = ServerBuilder.forPort(SetupLoader.getInstance().getServer().getPort());
+		}
+
+		// Validate JWT on all requests
+		AuthorizationServerInterceptor interceptor = getInterceptor();
+		serverBuilder.intercept(interceptor);
+
+		serverBuilder.addService(new TemplateService());
+		this.server = serverBuilder.build().start();
+		logger.info("Server started, listening on " + SetupLoader.getInstance().getServer().getPort());
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				// Use stderr here since the logger may have been reset by its JVM shutdown hook.
+				logger.info("*** shutting down gRPC Server since JVM is shutting down");
+				TemplateServer.this.stop();
+				logger.info("*** server shut down");
+			}
+		});
+	}
+
+	private void stop() {
+		if (this.server != null) {
+			this.server.shutdown();
+		}
+	}
+
+	/**
+	 * Await termination on the main thread since the grpc library uses daemon threads.
+	 */
+	private void blockUntilShutdown() throws InterruptedException {
+		if (this.server != null) {
+			this.server.awaitTermination();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		if (args == null) {
+			throw new Exception("Arguments Not Found");
+		}
+		//
+		if (args.length == 0) {
+			throw new Exception("Arguments Must Be: [property file name]");
+		}
+		String setupFileName = args[0];
+		if(setupFileName == null || setupFileName.trim().length() == 0) {
+			throw new Exception("Setup File not found");
+		}
+		SetupLoader.loadSetup(setupFileName);
+		//	Validate load
+		SetupLoader.getInstance().validateLoad();
+		final TemplateServer server = new TemplateServer();
+		server.start();
+		server.blockUntilShutdown();
+	}
+
+}
