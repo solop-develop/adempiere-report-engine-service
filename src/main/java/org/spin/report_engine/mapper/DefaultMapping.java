4951ac5999562cd1789ce37c6e480186a3223f13
EdwinBetanc0urt <EdwinBetanc0urt@outlook.com>
2025-04-08 10:21:44 -0400

    fix build.

--- src/main/java/org/spin/report_engine/mapper/DefaultMapping.java
+++ src/main/java/org/spin/report_engine/mapper/DefaultMapping.java
@@ -32,7 +32,6 @@ import org.spin.report_engine.format.PrintFormatColumn;
 import org.spin.report_engine.format.PrintFormatItem;
 import org.spin.report_engine.util.RecordUtil;
 import org.spin.service.grpc.util.value.BooleanManager;
-import org.spin.service.grpc.util.value.NumberManager;
 
 /**
  * Default just return the same value of cell
@@ -62,7 +61,8 @@ public class DefaultMapping implements IColumnMapping {
 							String tableName = resultSet.getString(I_AD_Table.COLUMNNAME_TableName);
 							if(!Util.isEmpty(tableName, true)) {
 								MTable table = MTable.get(Env.getCtx(), tableName);
-								int valueId = NumberManager.getIntFromObject(value);
+								int valueId = resultSet.getInt(column.getColumnName());
+								// int valueId = NumberManager.getIntFromObject(value);
 								cell.withTableName(tableName);
 								if (RecordUtil.isValidId(valueId, table)) {
 									PO entity = table.getPO(valueId, null);
